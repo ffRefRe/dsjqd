c45537eed913efd5c618802bc9f708fcb0c06320
FuJiaLiang <641734737@qq.com>
2018-12-05 21:53:40 +0800

    express

---
+++ server/db.js
@@ -0,0 +1,22 @@
+// 数据库连接配置
+// module.exports = {
+//   mysql: {
+//     host: '192.168.2.188',
+//     user: 'root',
+//     password: 'hadoop',
+//     database: 'fortify',
+//     port: '3306'
+//   }
+// }
+
+// 数据库连接配置
+module.exports = {
+  mysql: {
+    host: '62.234.202.151',
+    user: 'root',
+    password: '123456',
+    database: 'TDWeb',
+    port: '3306'
+  }
+}
+
