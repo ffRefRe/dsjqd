0b579998f7418a89b0650b32dfe109ad02115b84
FuJiaLiang <641734737@qq.com>
2018-12-18 14:04:42 +0800

    change -> changeOrigin: true,

--- config/index.js
+++ config/index.js
@@ -22,7 +22,6 @@ module.exports = {
     proxyTable: {
       '/api' : {
         target: 'http://127.0.0.1:3000/api/',
-        changeOrigin: true,
         pathRewrite: {
           '^/api': ''
         }
