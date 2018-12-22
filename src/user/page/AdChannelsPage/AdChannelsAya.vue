235e53ded060351664ced034e12a38c6beeb2d90
SuSunSam <339341037@qq.com>
2018-12-23 00:36:54 +0800

    ad_channels_page

--- src/user/page/AdChannelsPage/AdChannelsAya.vue
+++ src/user/page/AdChannelsPage/AdChannelsAya.vue
@@ -1,18 +1,43 @@
 <template>
 <div id="ad-channels-aya">
-  <div class="panel">
-    <div class="panel-heading">
-      <h3 class="panel-title">广告投放数据分析</h3>
-      <div class="right">
-        <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
-        <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+  <div class="chart_pannel">
+    <div class="panel">
+      <div class="panel-heading">
+        <h3 class="panel-title">广告投放数据分析</h3>
+        <div class="right">
+          <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+          <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+        </div>
+      </div>
+      <div class="chart-content"><ad-channel-day></ad-channel-day></div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="panel">
+        <div class="panel-heading">
+          <h3 class="panel-title">广告投放数据分析</h3>
+          <div class="right">
+            <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+            <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+          </div>
+        </div>
+      <div class="chart-content"><ad-channel-double-x></ad-channel-double-x></div>
       </div>
     </div>
-    <ad-channel-double-x></ad-channel-double-x>
-    <ad-channel-day></ad-channel-day>
-    <rean-time></rean-time>
+  <div class="col-md-6">
+    <div class="panel">
+      <div class="panel-heading">
+        <h3 class="panel-title">广告投放数据分析</h3>
+        <div class="right">
+          <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+          <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+        </div>
+      </div>
+      <div class="chart-content"><rean-time></rean-time></div>
+    </div>
   </div>
 </div>
+
 </template>
 
 <script>
@@ -28,5 +53,10 @@ export default {
 </script>
 
 <style scoped>
-
+  #ad-channels-aya .chart_pannel{
+    padding:0 15px !important;
+  }
+  #ad-channels-aya .chart-content{
+  padding:0 20px;
+}
 </style>
